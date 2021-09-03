541013188bf91317a56328c3d071664a3742aac1
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-09-03 18:02:51 +0300

    upd

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ app.init = async () => {
         user: 'root',
         database: 'barsukas',
     });
-    // paruosti reikiamus failus
+
+    let sql = '';
+    let rows = [];
+
+    // perskaitom ka turim is pradziu
+    sql = 'SELECT * \
+    FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // irasom nauja masina
+    sql = 'INSERT INTO `cars` \
+        (`id`, `marke`, `modelis`, `color`, `engine`, `doors`) \
+        VALUES (5, "Audi", "100", "grey", "2.2", "3")';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // atnaujiname
+    sql = 'UPDATE `cars` \
+        SET `marke` = "Opel", \
+            `modelis` = "Zafyra", \
+            `color` = "grey" \
+        WHERE `cars`.`id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // perskaitom ka turim po atnaujinimo
+    sql = 'SELECT * \
+    FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // istrinam
+    sql = 'DELETE FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
 
     // inicijuojame serveri
     server.init(connection);
