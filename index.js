eb7d71cddce55cec11545652373e6447c0de901f
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-09-11 09:15:33 +0300

    upd

--- index.js
+++ index.js
@@ -17,38 +17,38 @@ app.init = async () => {
     // perskaitom ka turim is pradziu
     sql = 'SELECT * \
     FROM `cars` \
-    WHERE `id` = 5';
+    WHERE `id` = 2';
     [rows] = await connection.execute(sql);
     console.log(rows);
 
-    // irasom nauja masina
+    //irasom nauja masina
     sql = 'INSERT INTO `cars` \
-        (`id`, `marke`, `modelis`, `color`, `engine`, `doors`) \
+        (`id`, `marke`, `modelis`, `spalva`, `variklis`, `durys`) \
         VALUES (5, "Audi", "100", "grey", "2.2", "3")';
     [rows] = await connection.execute(sql);
     console.log(rows);
 
-    // atnaujiname
-    sql = 'UPDATE `cars` \
-        SET `marke` = "Opel", \
-            `modelis` = "Zafyra", \
-            `color` = "grey" \
-        WHERE `cars`.`id` = 5';
-    [rows] = await connection.execute(sql);
-    console.log(rows);
+    // // atnaujiname
+    // sql = 'UPDATE `cars` \
+    //     SET `marke` = "Opel", \
+    //         `modelis` = "Zafyra", \
+    //         `color` = "grey" \
+    //     WHERE `cars`.`id` = 5';
+    // [rows] = await connection.execute(sql);
+    // console.log(rows);
 
     // perskaitom ka turim po atnaujinimo
     sql = 'SELECT * \
     FROM `cars` \
-    WHERE `id` = 5';
+    WHERE `id` = 2';
     [rows] = await connection.execute(sql);
     console.log(rows);
 
-    // istrinam
-    sql = 'DELETE FROM `cars` \
-    WHERE `id` = 5';
-    [rows] = await connection.execute(sql);
-    console.log(rows);
+    // // istrinam
+    // sql = 'DELETE FROM `cars` \
+    // WHERE `id` = 3';
+    // [rows] = await connection.execute(sql);
+    // console.log(rows);
 
     // inicijuojame serveri
     server.init(connection);
